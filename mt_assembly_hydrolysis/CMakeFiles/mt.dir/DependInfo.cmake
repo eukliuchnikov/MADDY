d2ae8e4bba241846860f63353359fbbff70e2292
eukliuchnikov <162615951+eukliuchnikov@users.noreply.github.com>
2024-04-05 13:46:21 -0400

    Add files via upload

---
+++ mt_assembly_hydrolysis/CMakeFiles/mt.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/configreader.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/configreader.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/dcdio.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/dcdio.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/globals.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/globals.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/main.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/main.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/parameters.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/parameters.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/pdbio.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/pdbio.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/preparator.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/preparator.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/timer.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/timer.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/updater.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/updater.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/wrapper.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/wrapper.cpp.o"
+  "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/src/xyzio.cpp" "/home/klyuchnikov/Desktop/PROJECTS/MT/mt_assembly_hydrolysis/CMakeFiles/mt.dir/src/xyzio.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/local/cuda-10.0/include"
+  "CUDA_SDK_ROOT_DIR-NOTFOUND/common/inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mt_assembly_hydrolysis/CMakeFiles/mt.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mt.dir/src/mt_generated_compute_cuda.cu.o"
+  "CMakeFiles/mt.dir/src/mt_generated_bdhitea.cu.o"
+  "CMakeFiles/mt.dir/src/mt_generated_bdhitea_kernel.cu.o"
+  "CMakeFiles/mt.dir/src/mt_generated_HybridTaus.cu.o"
+  "CMakeFiles/mt.dir/mt_intermediate_link.o"
+  "CMakeFiles/mt.dir/src/dcdio.cpp.o"
+  "CMakeFiles/mt.dir/src/xyzio.cpp.o"
+  "CMakeFiles/mt.dir/src/globals.cpp.o"
+  "CMakeFiles/mt.dir/src/main.cpp.o"
+  "CMakeFiles/mt.dir/src/pdbio.cpp.o"
+  "CMakeFiles/mt.dir/src/configreader.cpp.o"
+  "CMakeFiles/mt.dir/src/wrapper.cpp.o"
+  "CMakeFiles/mt.dir/src/timer.cpp.o"
+  "CMakeFiles/mt.dir/src/parameters.cpp.o"
+  "CMakeFiles/mt.dir/src/updater.cpp.o"
+  "CMakeFiles/mt.dir/src/preparator.cpp.o"
+  "mt.pdb"
+  "mt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
